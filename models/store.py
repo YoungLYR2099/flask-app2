efb9d78cb94972366617e7fb48122040f6803cc4
Yirui Liu <liuyiruiscu@gmail.com>
2018-12-22 13:57:35 -0800

    add all files

---
+++ models/store.py
@@ -0,0 +1,34 @@
+from db import db 
+
+class StoreModel(db.Model):
+	__tablename__ = 'stores'
+	
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80))
+	
+	items = db.relationship('ItemModel', lazy='dynamic')
+
+
+
+	def __init__(self,name,price):
+			self.name = name 
+			self.price = price 
+
+	def json(self):
+		return {'name': self.name, "items": [item.json for item in self.items]}
+
+	@classmethod
+	def find_by_name(cls,name):
+	    return cls.query.filter_by(name=name).first() 
+
+	def save_to_db(self):#update and insert 
+	    db.session.add(self)
+	    db.session.commit()
+
+	def delete_from_db(self):
+		db.session.delete(self)
+		db.session.commit()
+
+
+
+	    
